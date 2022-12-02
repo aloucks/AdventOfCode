edb8e781aa1e797e76d7527120c78d45fe720f37
Jeff Weber <jweber@cofront.net>
2022-12-02 01:28:42 -0500

    2022 day02 refactor

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -1,24 +1,26 @@
-use std::str::FromStr;
-
 fn main() -> utility::Result<()> {
     let input_file = utility::get_input_path()?;
     let input = utility::get_file_as_vec_string(&input_file)?;
 
     let input_1 = input
         .iter()
-        .map(|l| line_to_rps_vec(l))
-        .collect::<Vec<Vec<RPS>>>();
+        .map(|l| line_to_rps_tuple(l))
+        .collect::<Vec<(RPS, RPS)>>();
 
-    let score: u32 = input_1.iter().map(|l| rps_vec_to_score(l)).sum();
+    let score: u32 = input_1.iter().map(rps_tuple_to_score).sum();
 
     println!("{:?}", score);
 
     let input_1 = input
         .iter()
-        .map(|l| line_to_rps2_vec(l))
-        .collect::<Vec<Vec<RPS2>>>();
+        .map(|l| line_to_rps2_tuple(l))
+        .collect::<Vec<(RPS2, RPS2)>>();
 
-    let score: u32 = input_1.iter().map(|l| rps2_vec_to_score(l)).sum();
+    let score: u32 = input_1
+        .iter()
+        .map(rps2_tuple_to_rps_tuple)
+        .map(|l| rps_tuple_to_score(&l))
+        .sum();
 
     println!("{:?}", score);
 
@@ -32,61 +34,40 @@ enum RPS {
     Scissors,
 }
 
-impl FromStr for RPS {
-    type Err = utility::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" => Ok(RPS::Rock),
-            "B" => Ok(RPS::Paper),
-            "C" => Ok(RPS::Scissors),
-            "X" => Ok(RPS::Rock),
-            "Y" => Ok(RPS::Paper),
-            "Z" => Ok(RPS::Scissors),
-            _ => Err(utility::format_err!("unknown value: {}", s)),
+impl From<char> for RPS {
+    fn from(c: char) -> Self {
+        match c {
+            'A' => RPS::Rock,
+            'B' => RPS::Paper,
+            'C' => RPS::Scissors,
+            'X' => RPS::Rock,
+            'Y' => RPS::Paper,
+            'Z' => RPS::Scissors,
+            unk => panic!("unknown value {}", unk),
         }
     }
 }
 
-fn line_to_rps_vec(input: &str) -> Vec<RPS> {
-    let plays = input
-        .split_ascii_whitespace()
-        .map(|p| p.parse().unwrap())
-        .collect::<Vec<RPS>>();
-
-    plays
+fn line_to_rps_tuple(input: &str) -> (RPS, RPS) {
+    let play: (RPS, RPS) = (
+        input.chars().next().unwrap().into(),
+        input.chars().last().unwrap().into(),
+    );
+    play
 }
 
-fn rps_vec_to_score(input: &Vec<RPS>) -> u32 {
-    if input[0] == input[1] {
-        return rps_to_value(&input[1]) + 3;
+fn rps_tuple_to_score(input: &(RPS, RPS)) -> u32 {
+    match input {
+        (RPS::Rock, RPS::Rock) => 3 + rps_to_value(&RPS::Rock),
+        (RPS::Rock, RPS::Paper) => 6 + rps_to_value(&RPS::Paper),
+        (RPS::Rock, RPS::Scissors) => rps_to_value(&RPS::Scissors),
+        (RPS::Paper, RPS::Rock) => rps_to_value(&RPS::Rock),
+        (RPS::Paper, RPS::Paper) => 3 + rps_to_value(&RPS::Paper),
+        (RPS::Paper, RPS::Scissors) => 6 + rps_to_value(&RPS::Scissors),
+        (RPS::Scissors, RPS::Rock) => 6 + rps_to_value(&RPS::Rock),
+        (RPS::Scissors, RPS::Paper) => rps_to_value(&RPS::Paper),
+        (RPS::Scissors, RPS::Scissors) => 3 + rps_to_value(&RPS::Scissors),
     }
-
-    if input[0] == RPS::Rock && input[1] == RPS::Paper {
-        return 6 + rps_to_value(&input[1]);
-    }
-
-    if input[0] == RPS::Rock && input[1] == RPS::Scissors {
-        return 0 + rps_to_value(&input[1]);
-    }
-
-    if input[0] == RPS::Paper && input[1] == RPS::Rock {
-        return 0 + rps_to_value(&input[1]);
-    }
-
-    if input[0] == RPS::Paper && input[1] == RPS::Scissors {
-        return 6 + rps_to_value(&input[1]);
-    }
-
-    if input[0] == RPS::Scissors && input[1] == RPS::Rock {
-        return 6 + rps_to_value(&input[1]);
-    }
-
-    if input[0] == RPS::Scissors && input[1] == RPS::Paper {
-        return 0 + rps_to_value(&input[1]);
-    }
-
-    0
 }
 
 fn rps_to_value(input: &RPS) -> u32 {
@@ -107,82 +88,39 @@ enum RPS2 {
     Win,
 }
 
-impl FromStr for RPS2 {
-    type Err = utility::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "A" => Ok(RPS2::Rock),
-            "B" => Ok(RPS2::Paper),
-            "C" => Ok(RPS2::Scissors),
-            "X" => Ok(RPS2::Lose),
-            "Y" => Ok(RPS2::Draw),
-            "Z" => Ok(RPS2::Win),
-            _ => Err(utility::format_err!("unknown value: {}", s)),
+impl From<char> for RPS2 {
+    fn from(c: char) -> Self {
+        match c {
+            'A' => RPS2::Rock,
+            'B' => RPS2::Paper,
+            'C' => RPS2::Scissors,
+            'X' => RPS2::Lose,
+            'Y' => RPS2::Draw,
+            'Z' => RPS2::Win,
+            unk => panic!("unknown value {}", unk),
         }
     }
 }
 
-fn line_to_rps2_vec(input: &str) -> Vec<RPS2> {
-    let plays = input
-        .split_ascii_whitespace()
-        .map(|p| p.parse().unwrap())
-        .collect::<Vec<RPS2>>();
-
-    plays
+fn line_to_rps2_tuple(input: &str) -> (RPS2, RPS2) {
+    let play: (RPS2, RPS2) = (
+        input.chars().next().unwrap().into(),
+        input.chars().last().unwrap().into(),
+    );
+    play
 }
 
-fn rps2_vec_to_score(input: &Vec<RPS2>) -> u32 {
-    if input[1] == RPS2::Win {
-        if input[0] == RPS2::Rock {
-            return 6 + rps2_to_value(&RPS2::Paper);
-        }
-
-        if input[0] == RPS2::Paper {
-            return 6 + rps2_to_value(&RPS2::Scissors);
-        }
-
-        if input[0] == RPS2::Scissors {
-            return 6 + rps2_to_value(&RPS2::Rock);
-        }
-    }
-
-    if input[1] == RPS2::Lose {
-        if input[0] == RPS2::Rock {
-            return 0 + rps2_to_value(&RPS2::Scissors);
-        }
-
-        if input[0] == RPS2::Paper {
-            return 0 + rps2_to_value(&RPS2::Rock);
-        }
-
-        if input[0] == RPS2::Scissors {
-            return 0 + rps2_to_value(&RPS2::Paper);
-        }
-    }
-
-    if input[1] == RPS2::Draw {
-        if input[0] == RPS2::Rock {
-            return 3 + rps2_to_value(&RPS2::Rock);
-        }
-
-        if input[0] == RPS2::Paper {
-            return 3 + rps2_to_value(&RPS2::Paper);
-        }
-
-        if input[0] == RPS2::Scissors {
-            return 3 + rps2_to_value(&RPS2::Scissors);
-        }
-    }
-
-    0
-}
-
-fn rps2_to_value(input: &RPS2) -> u32 {
+fn rps2_tuple_to_rps_tuple(input: &(RPS2, RPS2)) -> (RPS, RPS) {
     match input {
-        RPS2::Rock => 1,
-        RPS2::Paper => 2,
-        RPS2::Scissors => 3,
-        _ => 0,
+        (RPS2::Rock, RPS2::Lose) => (RPS::Rock, RPS::Scissors),
+        (RPS2::Rock, RPS2::Draw) => (RPS::Rock, RPS::Rock),
+        (RPS2::Rock, RPS2::Win) => (RPS::Rock, RPS::Paper),
+        (RPS2::Paper, RPS2::Lose) => (RPS::Paper, RPS::Rock),
+        (RPS2::Paper, RPS2::Draw) => (RPS::Paper, RPS::Paper),
+        (RPS2::Paper, RPS2::Win) => (RPS::Paper, RPS::Scissors),
+        (RPS2::Scissors, RPS2::Lose) => (RPS::Scissors, RPS::Paper),
+        (RPS2::Scissors, RPS2::Draw) => (RPS::Scissors, RPS::Scissors),
+        (RPS2::Scissors, RPS2::Win) => (RPS::Scissors, RPS::Rock),
+        _ => panic!("unknown combo"),
     }
 }
